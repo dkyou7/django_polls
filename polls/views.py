06a2625361b87e1fa05dfd5a960d414926dbfc7b
youdongkwan <dkyou7@naver.com>
2020-02-12 17:48:34 +0900

    3.2.13 form 을 통해 투표 기능 구현하기

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -28,9 +28,26 @@ def detail(request,question_id):
 
 # 투표 결과 : 선택한 답변 반영 후 결과 보여줌
 def results(request,question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 
 # 투표 기능 : 선택한 답변에 따라 +1해줌
 def vote(request,question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',
+                      {
+                            'question':question,
+                            'error_message':"아무것도 선택하지 않으셨습니다.",
+                       }
+                      )
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))

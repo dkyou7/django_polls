94d40ef0e71a89cd88f9f9a34060fd4aab1b0cbd
youdongkwan <dkyou7@naver.com>
2020-02-13 09:37:33 +0900

    init project 완료

--- polls/views.py
+++ polls/views.py
@@ -30,16 +30,14 @@ def vote(request,question_id):
     # return HttpResponse("You're voting on question %s." % question_id)
     question = get_object_or_404(Question,pk=question_id)
     try:
-        selected_choice=question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError,Choice.DoesNotExist):
-        return render(request,'polls/detail.html',
-                      {
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
                             'question':question,
                             'error_message':"아무것도 선택하지 않으셨습니다.",
-                       }
-                      )
+                       })
     else:
-        selected_choice.votes +=1
+        selected_choice.votes += 1
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
